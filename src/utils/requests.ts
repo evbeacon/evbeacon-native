54291346936af7c486bcdefb2588facdb66d1868
Josh-McFarlin <jmcfarlin6@gatech.edu>
2020-10-11 16:29:38 -0400

    Create all API request functions

--- src/utils/requests.ts
+++ src/utils/requests.ts
@@ -55,3 +55,69 @@ export async function apiPostRequest<BodyType, ResponseType>(
 
   return json.payload;
 }
+
+export async function apiPatchRequest<BodyType, ResponseType>(
+  apiRoute: string,
+  body: BodyType
+): Promise<ResponseType> {
+  const response = await fetch(`${API_URL}${apiRoute}`, {
+    method: "PATCH",
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(body),
+  });
+
+  const json = await response.json();
+
+  if (json == null) {
+    throw new Error("Could not connect to API!");
+  } else if (!json.success) {
+    throw new Error(json.message);
+  }
+
+  return json.payload;
+}
+
+export async function apiPutRequest<BodyType>(
+  apiRoute: string,
+  body: BodyType
+): Promise<void> {
+  const response = await fetch(`${API_URL}${apiRoute}`, {
+    method: "PUT",
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(body),
+  });
+
+  if (!response.ok) {
+    throw new Error(response.statusText);
+  }
+}
+
+export async function apiDeleteRequest<BodyType, ResponseType>(
+  apiRoute: string,
+  body: BodyType
+): Promise<ResponseType> {
+  const response = await fetch(`${API_URL}${apiRoute}`, {
+    method: "DELETE",
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(body),
+  });
+
+  const json = await response.json();
+
+  if (json == null) {
+    throw new Error("Could not connect to API!");
+  } else if (!json.success) {
+    throw new Error(json.message);
+  }
+
+  return json.payload;
+}
